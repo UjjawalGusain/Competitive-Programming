ae5711af138780910e76aa8376692f18df6a827b
UjjawalGusain <ujjawalgusain31@gmail.com>
2024-03-23 07:07:06 +0530

    Add bit++ file

---
+++ Implementation/bit_plus_plus_1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, res = 0;
+    cin >> n;
+    for(int i = 0; i<n; i++)
+    {
+        string s;
+        cin >> s;
+        if(s == "X++" || s == "++X")
+            res++;
+        else
+            res--;
+    }
+    cout << res;
+    return 0;
+}
